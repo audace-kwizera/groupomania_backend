336fcb10d5cca6f112dc27662c109d92252d6a0b
audace-kwizera <companyfreshbeauty@gmail.com>
2021-09-25 21:31:51 +0200

    ajout des references dans la migration message

--- migrations/20210925192312-create-message.js
+++ migrations/20210925192312-create-message.js
@@ -10,7 +10,11 @@ module.exports = {
       },
       usersId: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          models: 'Users',
+          key: 'id'
+        }
       },
       title: {
         allowNull: false,
