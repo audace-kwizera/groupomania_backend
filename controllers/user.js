30a9d67facb75add40da604e2e5c5a5cd12e2310
audace-kwizera <companyfreshbeauty@gmail.com>
2021-09-26 12:42:13 +0200

    creation du serveeur et début de la configuration de la route user

---
+++ controllers/user.js
@@ -0,0 +1,14 @@
+/** Importation du hash */
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+/** Middleware enregistrement nouvel utilisateur */
+exports.register = (req, res, next) => {
+
+}
+
+/** Middleware connexion utilisateur */
+exports.login = (req, res, next) => {
+
+}
+
---
+++ routes/usersCtrl.js
@@ -0,0 +1,16 @@
+/** Importation de express */
+const express = require('express');
+
+/** Création d'un router avec la méthode router d'express */
+const router = express.Router();
+
+/** Importation des users */
+const userCtrl = require('../controllers/user');
+
+/** Création des routes post car le front end 
+ * enverra l'addresse mail et le mot de passe 
+ * */
+ router.post('/register', userCtrl.register);
+ router.post('/login', userCtrl.login);
+
+module.exports = router;
