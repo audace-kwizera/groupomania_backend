1c8384a7439bfa5821bc5fb65e7b74c4aa6f4b97
audace-kwizera <companyfreshbeauty@gmail.com>
2021-09-26 12:21:51 +0200

    ajout body-parser

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
+/** Importation bodyParser */
+const bodyParser = require('body-parser');
+
+/** Création de l'application express */
 const app = express();
 
+/** Importation de dotenv */
 require('dotenv').config()
 
+/** Export de notre application express */
 module.exports = app; 
